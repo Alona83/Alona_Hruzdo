e61fa34a5bcaab4a66db6f9b7124723d31268dc6
Alona83 <ogruzdo83@gmail.com>
2021-10-17 19:17:37 +0200

    new_case1

--- SQL_part2/Case1.sql
+++ SQL_part2/Case1.sql
@@ -6,19 +6,19 @@ SELECT p.product_id,
                '2018'
        END AS last_four_letters,
                  CASE
-           WHEN SUM(ISNULL((oi.list_price - oi.list_price * oi.discount), 0.0)) > 5000 THEN
-               'Excellent'
-           WHEN SUM(ISNULL((oi.list_price - oi.list_price * oi.discount), 0.0)) < 2000 THEN
-               'Bad'
            WHEN SUM(ISNULL((oi.list_price - oi.list_price * oi.discount), 0.0)) = 0 THEN
                'No sales'
+           WHEN SUM(ISNULL((oi.list_price - oi.list_price * oi.discount), 0.0)) < 2000 THEN
+               'Bad'
+           WHEN SUM(ISNULL((oi.list_price - oi.list_price * oi.discount), 0.0)) > 5000 THEN
+               'Excellent'
            ELSE
                'Good'
        END AS Product_category
 FROM [sales].[orders] o
-INNER JOIN [sales].[order_items] oi
+RIGHT JOIN [sales].[order_items] oi
         ON o.order_id = oi.order_id
-    INNER JOIN [production].[products] p
+    RIGHT JOIN [production].[products] p
         ON p.product_id = oi.product_id
 WHERE (product_name LIKE '%Surly%' AND product_name LIKE '%_2018%')
 GROUP BY p.product_id,
