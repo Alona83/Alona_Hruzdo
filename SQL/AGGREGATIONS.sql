09c6be55922705dd9c89f72111cf3a58149f06e1
Alona <91726980+Alona83@users.noreply.github.com>
2021-10-25 17:39:58 +0200

    Delete AGGREGATIONS.sql

--- SQL/AGGREGATIONS.sql
+++
@@ -1,8 +0,0 @@
-SELECT model_year,
-       AVG([list_price]) AS avg_lisr_price
-FROM [production].[products]
-WHERE model_year IN  (2016, 2019)
-AND [list_price] >= 832.99
-GROUP BY model_year
-HAVING MIN([list_price]) >= 850
-ORDER BY model_year 
