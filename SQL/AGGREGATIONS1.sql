f3f1dcdcf71a493988c81f6c60a2b2530807ce64
Alona83 <ogruzdo83@gmail.com>
2021-10-25 17:35:24 +0200

    new_queries

--- SQL/AGGREGATIONS1.sql
+++ SQL/AGGREGATIONS1.sql
@@ -1,6 +1,6 @@
 SELECT model_year,
-       AVG([list_price]) AS avg_lisr_price
-FROM [production].[products]
+       AVG([list_price]) AS avg_list_price
+FROM production.products
 WHERE list_price >= 832.99
 GROUP BY model_year
 HAVING MIN([list_price]) > 850
--- SQL/JOINS1.sql
+++ SQL/JOINS1.sql
@@ -1,14 +1,14 @@
-select p.product_id, 
+SELECT p.product_id, 
 p.product_name, 
 p.model_year,
 
-AVG(ISNULL(oi.discount, 0.0)) AS avg_discount
-from [production].[products] p
-LEFT JOIN [sales].[order_items] oi
-        ON p.product_id = oi.product_id
-where p.model_year = 2016
+AVG(ISNULL([discount], 0)) AS avg_discount
+FROM production.products p
+LEFT JOIN sales.order_items s
+        ON p.product_id = s.product_id
+WHERE p.model_year = 2016
 
 
-group by p.product_id, p.product_name, p.model_year
-HAVING AVG(ISNULL(oi.discount, 0.0)) <= 0.105581 AND AVG(ISNULL(oi.discount, 0.0)) >=0
+GROUP BY p.product_id, p.product_name, p.model_year
+HAVING AVG(ISNULL([discount], 0)) >= 0 OR AVG(ISNULL([discount], 0.0)) = 0.105581
 ORDER BY p.product_id
